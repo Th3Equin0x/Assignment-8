61a8a00c18014e90db4a49c627182eafd3f81848
Th3Equin0x <sgruver22@gmail.com>
2019-10-19 01:30:30 -0500

    Did the thing

---
+++ Assignment 8/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager instance;
+    public GameObject boxSmallPrefab;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RainBox", 2, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RainBox()
+    {
+        float randomX = Random.Range(-31, -27);
+        float randomZ = Random.Range(13, 19);
+
+        Instantiate(boxSmallPrefab, new Vector3(randomX, 5f, randomZ), boxSmallPrefab.transform.rotation);
+    }
+
+    public void StopRain()
+    {
+        CancelInvoke();
+    }
+}
---
+++ Assignment 8/Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    public Canvas prompt;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void DisplayPrompt()
+    {
+        prompt.gameObject.SetActive(true);
+        StartCoroutine("HideDisplay");
+    }
+
+    IEnumerator HideDisplay()
+    {
+        yield return new WaitForSeconds(5);
+        prompt.gameObject.SetActive(false);
+        yield return null;
+    }
+}
